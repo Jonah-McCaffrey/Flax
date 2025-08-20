a89018caa678dc3c21980b150feea1dbffd15f07
Jonah McCaffrey <jonahmac234@google.com>
2025-08-20 20:41:57 +0100

    Fixed infinite recursion with hostsDir

--- lib/default.nix
+++ lib/default.nix
@@ -18,17 +18,17 @@ in rec {
     { nixpkgs, inputs, }:
     { src, hostsDir ? null, sysSet ? sysSet, globalModules ? [ ]
     , specialArgs ? { }, perSystem ? { }, flake ? { }, }:
-    let hostsDir = if hostsDir == null then src + /hosts else hostsDir;
+    let hostsDir' = if hostsDir == null then src + /hosts else hostsDir;
     in recursiveUpdate {
       # TODO: Implement perSystem
       nixosConfigurations = mkNixOS {
-        hosts = getFileNames hostsDir;
+        hosts = getFileNames hostsDir';
         systems = sysSet.nixos;
-        inherit hostsDir globalModules specialArgs inputs;
+        inherit hostsDir' globalModules specialArgs inputs;
       };
     } flake;
 
-  mkNixOS = { hosts, hostsDir, systems, globalModules, specialArgs, inputs, }:
+  mkNixOS = { hosts, hostsDir', systems, globalModules, specialArgs, inputs, }:
     listToAttrs (flatten (map (host:
       map (system:
         nameValuePair "${host}@${system}" (nixosSystem {
@@ -36,6 +36,6 @@ in rec {
             inherit system inputs;
             util = import ./util.nix lib;
           } specialArgs;
-          modules = [ (hostsDir + /${host}.nix) ] ++ globalModules;
+          modules = [ (hostsDir' + /${host}.nix) ] ++ globalModules;
         })) systems) hosts));
 }
