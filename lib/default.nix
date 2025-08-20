5139cefc4dafa7d4af6b86188e6ac3540aedf926
Jonah McCaffrey <jonahmac234@google.com>
2025-08-20 20:40:09 +0100

    Moved helper functions to util.nix, added conditional to hostsDir

--- lib/default.nix
+++ lib/default.nix
@@ -1,35 +1,34 @@
-{ lib, sysSet }:
+{ lib, sysSet, }:
 let
-  inherit (builtins) listToAttrs attrNames readDir;
-  inherit (lib) flatten removeSuffix nixosSystem nameValuePair recursiveUpdate;
+  inherit (builtins) listToAttrs;
+  inherit (lib) flatten nixosSystem nameValuePair recursiveUpdate;
+  inherit (import ./util.nix lib) getFileNames;
 in rec {
   # Imports (keeps namespace(?) of lib, e.g., lib.example not lib.util.example (I think?).)
-  # import = [ /util.nix ];
+  # imports = [ /util.nix ];
 
   # Specific imports (intermediary namespace e.g., lib.util.example)
-  # util = import ./util.nix;
+  # util = import ./util.nix lib;
 
   # add the contents of the Flax library to lib
-  mkLib = lib: lib.recursiveUpdate (lib) (import ./default.nix);
-
-  # Get list of nix file names from a given directory
-  nixFiles = dir: map (removeSuffix ".nix") (attrNames (readDir dir));
+  # mkLib = lib: lib.recursiveUpdate lib (import ./default.nix);
 
   mkFlake =
     # Function to generate the flake output
-    { nixpkgs, inputs }:
-    { src, hostsDir ? src+/hosts, sysSet ? sysSet, globalModules ? [ ]
-    , specialArgs ? { }, perSystem ? { }, flake ? { } }:
-    recursiveUpdate {
+    { nixpkgs, inputs, }:
+    { src, hostsDir ? null, sysSet ? sysSet, globalModules ? [ ]
+    , specialArgs ? { }, perSystem ? { }, flake ? { }, }:
+    let hostsDir = if hostsDir == null then src + /hosts else hostsDir;
+    in recursiveUpdate {
       # TODO: Implement perSystem
       nixosConfigurations = mkNixOS {
-        hosts = nixFiles hostsDir;
+        hosts = getFileNames hostsDir;
         systems = sysSet.nixos;
         inherit hostsDir globalModules specialArgs inputs;
       };
     } flake;
 
-  mkNixOS = { hosts, hostsDir, systems, globalModules, specialArgs, inputs }:
+  mkNixOS = { hosts, hostsDir, systems, globalModules, specialArgs, inputs, }:
     listToAttrs (flatten (map (host:
       map (system:
         nameValuePair "${host}@${system}" (nixosSystem {
--- lib/util.nix
+++ lib/util.nix
@@ -1,1 +1,14 @@
-lib: { enabled = { enable = true; }; }
+lib:
+let
+  inherit (builtins) attrNames readDir;
+  inherit (lib) removeSuffix;
+in rec {
+  # helper enabled value
+  enabled = { enable = true; };
+
+  # Get list of file names from a given directory (WITH extension)
+  getFiles = dir: attrNames (readDir dir);
+
+  # Get list of file names from a given directory (WITHOUT extension)
+  getFileNames = dir: map (removeSuffix ".nix") (getFiles dir);
+}
