85f291c6864f23d61ccc68ad23b9d2774f504013
Jonah McCaffrey <jonahmac234@googlemail.com>
2025-11-01 23:22:58 +0000

    Testing different method of merging outputs

--- lib/default.nix
+++ lib/default.nix
@@ -3,16 +3,19 @@
   systemSet,
 }: {
   # Function to generate the flake output
-  mkFlake =
-    {inputs}: {
-      imports ? [],
-      systems ? systemSet.default,
-      perSystem ? {},
-      flake ? {},
-    }:
-      flake
-    # Standard flake outputs
-    ;
+  mkFlake = {inputs}: {
+    imports ? [],
+    systems ? systemSet.default,
+    perSystem ? {},
+    flake ? {},
+  }:
+    lib.foldl' lib.recursiveUpdate {} (
+      imports # Flake modules
+      ++ [
+        (lib.genAttrs systems perSystem) # Per system outputs
+        flake # Standard flake outputs
+      ]
+    );
 
   # Import lib functions
   imports = [
