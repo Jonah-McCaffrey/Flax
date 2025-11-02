d8f63ac71d3dbf41c165cba87864b60b48d69bd7
Jonah McCaffrey <jonahmac234@googlemail.com>
2025-11-02 13:09:50 +0000

    Refactoring and simplifying

--- lib/default.nix
+++ lib/default.nix
@@ -5,21 +5,23 @@
   evalFlakeModule = args: module:
     lib.evalModules {
       specialArgs = {inputs = args.inputs;};
-      modules = module.imports;
+      modules = [module];
     };
 
   # Function to generate the flake output
-  mkFlake = args: {
-    imports ? [],
-    systems ? systemSet.default,
-    perSystem ? system: {},
-    flake ? {},
-  }:
-    lib.foldl' lib.recursiveUpdate {} (lib.flatten [
-      (map (module: (evalFlakeModule args module).config) imports) # Flake modules
-      (map perSystem systems) # Outputs defined per-system
-      flake # Standard flake outputs
-    ]);
+  # mkFlake = args: {
+  #   imports ? [],
+  #   systems ? systemSet.default,
+  #   perSystem ? system: {},
+  #   flake ? {},
+  # }:
+  #   lib.foldl' lib.recursiveUpdate {} (lib.flatten [
+  #     (map (module: (evalFlakeModule args module).config) imports) # Flake modules
+  #     (map perSystem systems) # Outputs defined per-system
+  #     flake # Standard flake outputs
+  #   ]);
+
+  mkFlake = args: module: (evalFlakeModule args module).config;
 
   # Import lib functions
   # imports = [
