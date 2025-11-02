073bb5bcba750ee900d85f36d2b00bcad2044dcc
Jonah McCaffrey <jonahmac234@googlemail.com>
2025-11-02 02:46:41 +0000

    NixOS module refactoring and inclusion of specialArgs

--- lib/default.nix
+++ lib/default.nix
@@ -1,16 +1,22 @@
 {
   lib,
   systemSet,
-}: {
+}: rec {
+  evalFlakeModule = args: module:
+    lib.evalModules {
+      specialArgs = {inputs = args.inputs;};
+      modules = imports;
+    };
+
   # Function to generate the flake output
-  mkFlake = {inputs}: {
+  mkFlake = args: {
     imports ? [],
     systems ? systemSet.default,
     perSystem ? system: {},
     flake ? {},
   }:
     lib.foldl' lib.recursiveUpdate {} (lib.flatten [
-      (lib.evalModules {modules = imports;}) # Flake modules
+      (map (module: evalFlakeModule args module) imports) # Flake modules
       (map perSystem systems) # Outputs defined per-system
       flake # Standard flake outputs
     ]);
